fef5572a70e062eeb67b8cee72390fb9a5bc1552
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2024-08-29 02:38:01 +0200

    spotless

--- src/main/java/micdoodle8/mods/galacticraft/core/asm/GCLoadingPlugin.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/asm/GCLoadingPlugin.java
@@ -1,6 +1,10 @@
 package micdoodle8.mods.galacticraft.core.asm;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
