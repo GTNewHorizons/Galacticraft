2abc4467337be8e2c5a725b96fc3fd22026238c7
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2025-07-13 22:38:16 +0200

    Convert to new IMixins and MixinsBuilder system

--- src/main/java/micdoodle8/mods/galacticraft/core/asm/GCLoadingPlugin.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/asm/GCLoadingPlugin.java
@@ -1,15 +1,11 @@
 package micdoodle8.mods.galacticraft.core.asm;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import com.gtnewhorizon.gtnhmixins.IEarlyMixinLoader;
+import com.gtnewhorizon.gtnhmixins.builders.IMixins;
 
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
 import micdoodle8.mods.galacticraft.core.Constants;
@@ -18,8 +14,6 @@ import micdoodle8.mods.galacticraft.core.mixins.Mixins;
 @IFMLLoadingPlugin.MCVersion("1.7.10")
 @IFMLLoadingPlugin.Name(Constants.COREMOD_NAME_SIMPLE)
 public class GCLoadingPlugin implements IFMLLoadingPlugin, IEarlyMixinLoader {
-
-    private static final Logger LOGGER = LogManager.getLogger(Constants.COREMOD_NAME_SIMPLE);
 
     @Override
     public String[] getASMTransformerClass() {
@@ -51,18 +45,6 @@ public class GCLoadingPlugin implements IFMLLoadingPlugin, IEarlyMixinLoader {
 
     @Override
     public List<String> getMixins(Set<String> loadedCoreMods) {
-        final List<String> mixins = new ArrayList<>();
-        final List<String> notLoading = new ArrayList<>();
-        for (final Mixins mixin : Mixins.values()) {
-            if (mixin.phase == Mixins.Phase.EARLY) {
-                if (mixin.shouldLoad(loadedCoreMods, Collections.emptySet())) {
-                    mixins.addAll(mixin.mixinClasses);
-                } else {
-                    notLoading.addAll(mixin.mixinClasses);
-                }
-            }
-        }
-        LOGGER.info("Not loading the following EARLY mixins: {}", notLoading.toString());
-        return mixins;
+        return IMixins.getEarlyMixins(Mixins.class, loadedCoreMods);
     }
 }
--- src/main/java/micdoodle8/mods/galacticraft/core/mixins/TargetedMod.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/mixins/TargetedMod.java
@@ -1,33 +1,24 @@
 package micdoodle8.mods.galacticraft.core.mixins;
 
-public enum TargetedMod {
+import javax.annotation.Nonnull;
 
-    VANILLA("Minecraft", null),
-    FORGE("Minecraft Forge", "net.minecraftforge.classloading.FMLForgePlugin", "Forge"),
-    PLAYERAPI("Player API", "api.player.forge.PlayerAPIPlugin", "PlayerAPI"),
-    OPTIFINE("Optifine", "optifine.OptiFineForgeTweaker", "Optifine");
+import com.gtnewhorizon.gtnhmixins.builders.ITargetMod;
+import com.gtnewhorizon.gtnhmixins.builders.TargetModBuilder;
 
-    public final String modName;
-    public final String coreModClass;
-    public final String modId;
+public enum TargetedMod implements ITargetMod {
 
-    TargetedMod(String modName, String coreModClass) {
-        this(modName, coreModClass, null);
+    PLAYERAPI("api.player.forge.PlayerAPIPlugin", "PlayerAPI"),
+    OPTIFINE("optifine.OptiFineForgeTweaker", "Optifine");
+
+    private final TargetModBuilder builder;
+
+    TargetedMod(String coreModClass, String modId) {
+        this.builder = new TargetModBuilder().setCoreModClass(coreModClass).setModId(modId);
     }
 
-    TargetedMod(String modName, String coreModClass, String modId) {
-        this.modName = modName;
-        this.coreModClass = coreModClass;
-        this.modId = modId;
-    }
-
+    @Nonnull
     @Override
-    public String toString() {
-        return "TargetedMod{modName='" + this.modName
-                + "', coreModClass='"
-                + this.coreModClass
-                + "', modId='"
-                + this.modId
-                + "'}";
+    public TargetModBuilder getBuilder() {
+        return builder;
     }
 }
