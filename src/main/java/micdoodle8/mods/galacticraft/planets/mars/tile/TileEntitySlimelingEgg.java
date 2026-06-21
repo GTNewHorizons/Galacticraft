efbf6e2644cdc1a4c77abbc6ea0e77b2bd7cf67e
Charsy89 <28523197+Charsy89@users.noreply.github.com>
2026-06-21 01:44:06 -0600

    Added missing method name comment

--- src/main/java/micdoodle8/mods/galacticraft/planets/mars/tile/TileEntitySlimelingEgg.java
+++ src/main/java/micdoodle8/mods/galacticraft/planets/mars/tile/TileEntitySlimelingEgg.java
@@ -47,7 +47,7 @@ public class TileEntitySlimelingEgg extends TileEntity {
                                 colorBlue);
 
                         slimeling.setPosition(this.xCoord + 0.5, this.yCoord + 1.0, this.zCoord + 0.5);
-                        slimeling.func_152115_b(this.lastTouchedPlayerUUID);
+                        slimeling.func_152115_b(this.lastTouchedPlayerUUID); // setOwner
                         slimeling.setOwnerUsername(this.lastTouchedPlayerName);
 
                         if (!this.worldObj.isRemote) {
