e6406a56e17b72be7632c855164973403a7af841
Pxx500 <pbartulik@gmail.com>
2026-06-21 12:53:45 +0200

    review

--- src/main/java/micdoodle8/mods/galacticraft/planets/asteroids/client/FluidTexturesGC.java
+++ src/main/java/micdoodle8/mods/galacticraft/planets/asteroids/client/FluidTexturesGC.java
@@ -2,10 +2,12 @@ package micdoodle8.mods.galacticraft.planets.asteroids.client;
 
 import net.minecraftforge.client.event.TextureStitchEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fluids.FluidRegistry;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import micdoodle8.mods.galacticraft.core.util.CompatibilityManager;
 import micdoodle8.mods.galacticraft.planets.asteroids.AsteroidsModule;
 
 @SideOnly(Side.CLIENT)
@@ -24,6 +26,26 @@ public class FluidTexturesGC {
                     .setIcons(event.map.registerIcon(AsteroidsModule.ASSET_PREFIX + ":fluids/LiquidMethane"));
             AsteroidsModule.fluidLiquidArgon
                     .setIcons(event.map.registerIcon(AsteroidsModule.ASSET_PREFIX + ":fluids/LiquidArgon"));
+            if (!CompatibilityManager.isGTLoaded()) {
+                AsteroidsModule.fluidMethaneGas
+                        .setIcons(event.map.registerIcon(AsteroidsModule.ASSET_PREFIX + ":fluids/MethaneGas"));
+                AsteroidsModule.fluidLiquidOxygen
+                        .setIcons(event.map.registerIcon(AsteroidsModule.ASSET_PREFIX + ":fluids/LiquidOxygen"));
+                AsteroidsModule.fluidOxygenGas
+                        .setIcons(event.map.registerIcon(AsteroidsModule.ASSET_PREFIX + ":fluids/OxygenGas"));
+                AsteroidsModule.fluidLiquidNitrogen
+                        .setIcons(event.map.registerIcon(AsteroidsModule.ASSET_PREFIX + ":fluids/LiquidNitrogen"));
+                AsteroidsModule.fluidNitrogenGas
+                        .setIcons(event.map.registerIcon(AsteroidsModule.ASSET_PREFIX + ":fluids/NitrogenGas"));
+                FluidRegistry.getFluid("hydrogen")
+                        .setIcons(event.map.registerIcon(AsteroidsModule.ASSET_PREFIX + ":fluids/HydrogenGas"));
+                FluidRegistry.getFluid("helium")
+                        .setIcons(event.map.registerIcon(AsteroidsModule.ASSET_PREFIX + ":fluids/HeliumGas"));
+                FluidRegistry.getFluid("argon")
+                        .setIcons(event.map.registerIcon(AsteroidsModule.ASSET_PREFIX + ":fluids/ArgonGas"));
+                FluidRegistry.getFluid("carbondioxide")
+                        .setIcons(event.map.registerIcon(AsteroidsModule.ASSET_PREFIX + ":fluids/CarbonDioxideGas"));
+            }
         }
     }
 }
